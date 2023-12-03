50e65a147dee16e4615b0a401a4ab546e6de0d68
Pacharatnkp <pachara.pplzz@gmail.com>
2023-12-03 17:08:22 +0700

    feat:answer-ex

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,11 @@
 let numbers = [100, 20, 3, 1000];
 let minNumber;
 // Start coding here
-
-console.log(minNumber);
+minNumber = numbers[0];
+for (let i = 1; i < numbers.length; i++) {
+    if (numbers[i] < minNumber) {
+        minNumber = numbers[i];
+    }
+  }
+  
+  console.log(minNumber);
